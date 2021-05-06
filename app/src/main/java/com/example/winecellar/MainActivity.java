1a5a096cd8593fed5805b1b51dbe2e03401c7d44
Pascal <pascal.konezciny@students.fhnw.ch>
2021-05-06 16:24:43 +0200

    Inital Commit

---
+++ app/src/main/java/com/example/winecellar/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.winecellar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    ImageButton addBtn, viewBtn, reportBtn, drinkBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        addBtn = findViewById(R.id.addBtn);
+        viewBtn = findViewById(R.id.viewBtn);
+        reportBtn = findViewById(R.id.reportBtn);
+        drinkBtn = findViewById(R.id.drinkBtn);
+
+
+
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddActivity.class);
+                startActivity(intent);
+            }
+        });
+        viewBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ViewActivity.class);
+                startActivity(intent);
+            }
+        });
+        reportBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DrinkActivity.class);
+                startActivity(intent);
+            }
+        });
+        drinkBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ReportActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
